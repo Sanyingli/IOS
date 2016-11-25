1c17e07fecf00623aafec57b54254eb2ca00b0c1
Sanying Li <sanyingli@gwu.edu>
2016-11-25 14:20:47 -0500

    project 2, Guess the flag

---
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by 李原平 on 16/11/25.
+//  Copyright © 2016年 李原平. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //countriesAppend()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        
+        askQuestion()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+/*
+    func countriesAppend()
+    {
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+        
+    }
+ */
+    func askQuestion(action: UIAlertAction! = nil)
+    {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries)as! [String]
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        var title: String
+        if sender.tag == correctAnswer
+        {
+            title = "Correct"
+            score += 1
+        }
+        else
+        {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "You score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        
+    }
+
+}
+
